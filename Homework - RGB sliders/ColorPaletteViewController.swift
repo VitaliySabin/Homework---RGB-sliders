275f97a969545792138494b3ac263037288f3880
Vitaliy Sbain <vitaliy-sabin@yandex.ru>
2023-03-26 19:36:30 +0300

    Small bug fixed

--- Homework - RGB sliders/ColorPaletteViewController.swift
+++ Homework - RGB sliders/ColorPaletteViewController.swift
@@ -22,6 +22,7 @@ final class ColorPaletteViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        mainView.layer.cornerRadius = 20
         updateLabels()
         updateViewBackground()
     }
