4b63aadecbbf0b3648543d21c2f710960ddd7bae
Vitaliy Sbain <vitaliy-sabin@yandex.ru>
2023-04-12 18:11:24 +0300

    work with storyboards

---
+++ Homework - RGB sliders/ViewControllers/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  Homework - RGB sliders
+//
+//  Created by Виталий Сабин on 12.04.2023.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func editButtonPressed() {
+    }
+}
---
+++ Homework - RGB sliders/ViewControllers/SettingsViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  Homework - RGB sliders
+//
+//  Created by Виталий Сабин on 24.03.2023.
+//
+
+import UIKit
+
+final class SettingsViewController: UIViewController {
+    
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        colorView.layer.cornerRadius = 20
+        setColor()
+    }
+    
+    @IBAction func sliderAction(_ sender: UISlider) {
+        setColor()
+        switch sender {
+        case redSlider:
+            redLabel.text = string(from: redSlider)
+        case greenSlider:
+            greenLabel.text = string(from: greenSlider)
+        default:
+            blueLabel.text = string(from: blueSlider)
+        }
+    }
+    
+    private func setColor() {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
+    
+    private func string(from slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
+    }
+}
